85621d727ce9894b4840040a09b51fea7869d5c4
wili1991 <wikki9106@gmail.com>
2023-02-13 21:27:31 -0500

    switch

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-let edad=prompt("digita el numero la edad");
-
+let animal=prompt("digita el nombre del carnivoro herbivoro omnivoro para saber de que se alimentan ");
 
 //CONDICIONAL SIMPLE IF
 
@@ -26,17 +25,35 @@ let edad=prompt("digita el numero la edad");
 //     console.log(`${b} es mayor ${a}`);    
 // }
 
-if (edad>=18 && edad<=29 ) {
-    console.log(`Tienes ${edad} años eres mayor, un joven mayor de edad`);
-} 
-else if (edad>=30 && edad<=49 ) {
-    console.log(`Tienes ${edad} años eres mayor, ya tienes experiencia`);
-}else if(edad>=50){
-    console.log(`Tienes ${edad} años eres mayor, ya tienes experiencia y debes cuidarte`);
-}
-else if(edad >1){
-    console.log(`Eres menor de edad tienes ${edad} años, aprovecha el tiempo`);
-}
-else{
-    console.log("Error ingresa nuevamente tu edad");
+
+//CONDICIONAL IF ... ELSE IF.. ELSE
+
+// if (edad>=18 && edad<=29 ) {
+//     console.log(`Tienes ${edad} años eres mayor, un joven mayor de edad`);
+// } 
+// else if (edad>=30 && edad<=49 ) {
+//     console.log(`Tienes ${edad} años eres mayor, ya tienes experiencia`);
+// }else if(edad>=50){
+//     console.log(`Tienes ${edad} años eres mayor, ya tienes experiencia y debes cuidarte`);
+// }
+// else if(edad >1){
+//     console.log(`Eres menor de edad tienes ${edad} años, aprovecha el tiempo`);
+// }
+// else{
+//     console.log("Error ingresa nuevamente tu edad");
+// }
+
+//CONDICIONAL SWITCH
+
+switch (animal.toLowerCase()) {
+    case "carnivoro":
+        console.log("Carnívoros: Son animales que se alimentan de carne, cazando su presa para luego comerla. Las garras de los animales carnívoros depredadores son importantes para agarrar a su presa y poder comerla con más facilidad.");
+        break;
+    case "herbivoro":
+        console.log("Herbivoro: Son animales que se alimentan exclusivamente de plantas, pastos, hierbas y vegetales en general. Hay muchos herbívoros que comen huevos y a veces otras proteínas provenientes de animales.");
+    case "omnivoro":
+    console.log("Omnivoro: Son animales que se alimentan de carne y plantas, por lo tanto son animales que comen toda clase de alimentos, por lo mismo pueden habitar fácilmente en cualquier ambiente.");
+    default:
+        console.log("ingrese nuevamente una de las opciones");
+        break;
 }
