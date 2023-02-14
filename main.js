df10eff842e22a661a718a75aa06829cf195dada
wili1991 <wikki9106@gmail.com>
2023-02-13 20:50:18 -0500

    condicional if

---
+++ main.js
@@ -0,0 +1,13 @@
+let a=prompt("digita el numero a");
+let b=prompt("digita el numero b");
+
+//CONDICIONAL SIMPLE IF
+
+// if (condition) {
+    
+// }
+
+if (a>b) {
+    console.log(`${a} es mayor que ${b}`);
+}
+
